06cf8c08d7ab2899f82d069721cbf7e5c3fc417e
TarinTuly <tarinn577@gmail.com>
2025-02-15 16:11:35 +0600

    user table migration

---
+++ Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Issue.Models
+{
+    public class UserContext :DbContext
+    {
+        public UserContext(DbContextOptions options):base(options) 
+        { 
+        }
+        public DbSet<Users>Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Issue.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; } // Store hashed password
+
+        [Required]
+        public string UserType { get; set; } // "Admin", "Developer", "Reporter"
+    }
+}
